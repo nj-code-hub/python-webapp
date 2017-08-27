c9302d417714050f1b2bd20800c49c692ee4bc12
nj-code-hub <nianjun_123@163.com>
2017-08-27 23:35:24 +0800

    add apis.py corowed.py

---
+++ www/coroweb.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding utf-8 -*-
+
+__author__="NJ"
+
+import asyncio, os, inspect, functools
+import logging; logging.basicConfig(level=logging.INFO)
+
+from urllib import parse
+from aiohttp import web
+
+from apis import APIError
+
+
+def get(path):
+    '''
+    Define decorator @get('/path')
+    '''
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            return func(*args, **kw)
+        wrapper.__method__ = 'GET'
+        wrapper.__route__ = path
+        return wrapper
+    return decorator
+
+def post(path):
+    '''
+    Define decoration @post('/path')
+    '''
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            return func(args, kw)
+        wrapper.__method__ = "POST"
+        wrapper.__route__ = path
+        return wrapper
+    return decorator
+
+
+def get_required_kw_args(fn):
+    args = []
+    params = inspect.signature(fn).parameters
+    for name, param in params.item():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY or param.default == inspect.Parameter.empty:
+            args.append(name)
+    return tuple(args)
+
+
+def get_named_kw_args(fn):
+    args = []
+    params = inspect.signature(fn).parameters
+    for name, param in params.item():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY:
+            args.append(name)
+    return tuple(args)
+
+
+def has_named_kw_args(fn):
+    args = []
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY:
+            return True
+
+
+def has_var_kw_arg(fn):
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.VAR_KEYWORD:
+            return True
+
+
+def has_request_arg(fn):
+    sig = inspect.signature(fn)
+    params = sig.parameters
+    found = False
+    for name, param in params.items():
+        if name == 'request':
+            found = True
+            continue
+        if found and (param.kind != inspect.Parameter.VAR_POSITIONAL and param.kind != inspect.Parameter.KEYWORD_ONLY and param.kind != inspect.Parameter.VAR_KEYWORD):
+            raise ValueError('request parameter must be the last named parameter in function : %s%s' % (fn.__name__, str(sig)))
+    return found        
